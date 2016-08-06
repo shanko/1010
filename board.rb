882c107304455e39137f0ee5915636c1c4072a62
Shashank Date <shashankdate@Shashanks-MacBook-Pro-2.local>
2016-08-06 10:03:26 -0500

    Game code version 1.0

---
+++ board.rb
@@ -0,0 +1,551 @@
+# board.rb
+
+class Board1010
+
+  attr_accessor :rows, :cols, :arr, :score, :options, :val, :placed_pos
+
+  MAX_ROWS    = 10
+  MAX_COLS    = 10
+  MAX_OPTIONS = 3
+  GAME_FILE_NAME = (RUBY_ENGINE == 'mruby') ? "./1010game.str" : "./1010game.dat"
+
+  DOT = [1]
+
+  # Horizontal Lines
+  HL = [[], [ 1], [2,  2], [3,  3, 3], [4,  4, 4, 4], [5,  5, 5, 5, 5]]
+
+  # Vertical Lines
+  VL = [[], [-1], [-1,-1], [-1,-1,-1], [-1,-1,-1,-1], [-1,-1,-1,-1,-1]]
+
+  # Squares
+  SQ = [
+    [],[[1]],
+    [[2,2],
+     [2,2]],
+    [[3,3,3],
+     [3,3,3],
+     [3,3,3]],
+  ]
+
+  # Bottom Left Corners
+  BL = [
+    [],[[1]],
+    [[2,0],
+     [2,2]],
+    [[3,0,0],
+     [3,0,0],
+     [3,3,3]],
+  ]
+
+  # Top Left Corners
+  TL = [
+    [],[[1]],
+    [[2,2],
+     [0,2]],
+    [[3,3,3],
+     [0,0,3],
+     [0,0,3]],
+  ]
+
+  # Bottom Right Corners
+  BR = [
+    [],[[1]],
+    [[0,2],
+     [2,2]],
+    [[0,0,3],
+     [0,0,3],
+     [3,3,3]],
+  ]
+
+  # Top Right Corners
+  TR = [
+    [],[[1]],
+    [[2,2],
+     [2,0]],
+    [[3,3,3],
+     [3,0,0],
+     [3,0,0]],
+  ]
+
+  ALL_TILES = [
+      DOT,
+
+      HL[2],
+      HL[3],
+      HL[4],
+      HL[5],
+
+      VL[2],
+      VL[3],
+      VL[4],
+      VL[5],
+
+      SQ[2],
+      SQ[3],
+
+      BL[2],
+      BL[3],
+
+      TL[2],
+      TL[3],
+
+      BR[2],
+      BR[3],
+
+      TR[2],
+      TR[3],
+  ]
+
+  def initialize(rows=MAX_ROWS, columns=MAX_COLS)
+    @rows    = rows
+    @cols    = columns
+    @arr     = nil
+    @score   = 0
+    @val     = 0
+    @options = []
+    @prev_options = @prev_score = @prev_arr = nil
+  end
+
+  def marshal_dump
+    [@score, @arr, @options]
+  end
+
+  def marshal_load(array)
+    @score, @arr, @options = array
+  end
+
+  def prev_arr
+    @prev_arr
+  end
+
+  def to_s
+     opt_str = ""
+     @options.each do |opt|
+       opt_str += opt.inject(''){|sum,c| sum + c.to_s}
+       opt_str += ','
+     end
+    "@rows=#{@rows};@cols=#{@cols};@score=#{@score};@val=#{@val};@options=#{@options};@arr=#{@arr}"
+  end
+
+  def parse_load(data)
+    bGame = Struct.new("Game",:rows,:cols,:score,:val,:options,:arr)
+    game = bGame.new
+    data.split(';').each do |element|
+      key,val = element.split('=')
+      case key
+      when "@rows"
+        game.rows = val.to_i
+      when "@cols"
+        game.cols = val.to_i
+      when "@score"
+        game.score = val.to_i
+      when "@val"
+        game.val = val.to_i
+      when "@options"
+        game.options = [[1],[1,1,1],[-1,-1,-1]]
+      when "@arr"
+        game.arr = Array.new(game.rows){Array.new(game.cols){@val}}
+      end
+    end
+    game
+  end
+
+  def load_game
+    game = nil
+    if File.exist?(GAME_FILE_NAME)
+      File.open(GAME_FILE_NAME,"rb") do |f|
+        data = f.read
+        if RUBY_ENGINE == 'mruby'
+          game = parse_load(data)
+        else
+          game = Marshal::load(data)
+        end
+      end
+    end
+    game
+  end
+
+  def save
+    print "Do you want to save the game to play it later? [y/n] "
+    opt = gets.chomp.downcase
+    if(opt == "y")
+      File.open(GAME_FILE_NAME,"wb") do |f|
+        if (RUBY_ENGINE == 'mruby')
+          f.write(self.to_s)
+        else
+          f.write(Marshal::dump(self))
+        end
+      end
+    end
+    (opt == "y")
+  end
+
+  def init(val=nil)
+    if val.class == Array
+      @arr   = val
+      @score = _filled_cells
+    elsif val.class == Fixnum
+      @val = val
+      @arr = Array.new(@rows){Array.new(@cols){val}}
+      @score = 0
+      @options = []
+    elsif (game = load_game)
+      @rows    = Board1010::MAX_ROWS
+      @cols    = Board1010::MAX_COLS
+      @val     = 0
+      @arr     = game.arr
+      @score   = game.score
+      @options = game.options
+    else
+      @arr = Array.new(@rows){Array.new(@cols){@val}}
+    end
+
+    start
+  end
+
+  def current_score
+    @score
+  end
+
+  def restore_arr(prev_arr)
+    @arr = Marshal.load(Marshal.dump(prev_arr))
+  end
+
+  def restore_options(prev_options)
+    @options = Marshal.load(Marshal.dump(prev_options))
+  end
+
+  def cell=(triplet)
+    i,j,val = triplet
+    _set_cell(i,j,val)
+  end
+
+  def get_selected_option(i,j)
+    puts "Select one option for position (#{i}, #{j}):"
+    while true do
+      print "option> "
+      option = gets.chomp
+      stop if option.to_s.downcase == "q"
+      return option.to_i if [1,2,3].include?(option.to_i)
+      puts "Invalid option #{option}. Should be one of [1,2,3]"
+    end
+  end
+
+
+  def get_position
+    print "Select Row, Col: "
+    line = gets.chomp
+    x,y  = line.split(',')
+    stop if x.to_i < 0 || y.to_i < 0
+    i,j  = x.to_i % Board1010::MAX_ROWS, y.to_i % Board1010::MAX_COLS
+    [i,j]
+  end
+
+  def row_cleanup(i)
+    @cols.times do |j|
+      _set_cell(i,j,@val)
+    end
+  end
+
+  def col_cleanup(j)
+    @rows.times do |i|
+      _set_cell(i,j,@val)
+    end
+  end
+
+  def cleanup(dry_run=false)
+    rows_to_clean = []
+    @rows.times do |i|
+      row_full = true
+      @cols.times do |j|
+        if _cell_empty?(i,j)
+          row_full = false
+          break
+        end
+      end
+      rows_to_clean << i if row_full
+    end
+
+    cols_to_clean = []
+    @cols.times do |j|
+      col_full = true
+      @rows.times do |i|
+        if _cell_empty?(i,j)
+          col_full = false
+          break
+        end
+      end
+      cols_to_clean << j if col_full
+    end
+
+    unless dry_run
+      rows_to_clean.each {|i| row_cleanup(i)}
+      cols_to_clean.each {|j| col_cleanup(j)}
+    end
+
+    [rows_to_clean, cols_to_clean]
+  end
+
+  def backtrack
+    @placed_pos.each do |i,j,k|
+      _set_cell(i,j,0)
+    end
+    @placed_pos = []
+  end
+
+  def place(tile,i,j,dry_run=false)
+    score = 0
+    @placed_pos = []
+    tile.each_with_index do |cell,r|
+      if (cell.class == Array)
+        if r == 0
+          cell.each do |ele|
+            break if ele > 0
+            j -= 1
+          end
+          return 0 if j < 0
+        end
+        cell.each_with_index do |ele, c|
+          if ele > 0 && _cell_occupied?(i+r, j+c)
+            backtrack unless dry_run
+            return 0
+          end
+          if ele > 0
+            unless dry_run
+              _set_cell(i+r,j+c,ele)
+            end
+            @placed_pos << [i+r,j+c,ele]
+            score += 1
+          end
+        end
+      elsif cell > 0
+        if _cell_occupied?(i, j+r)
+          backtrack unless dry_run
+          return 0
+        end
+        unless dry_run
+          _set_cell(i,j+r,cell)
+        end
+        @placed_pos << [i,j+r,cell]
+        score += 1
+      elsif cell < 0
+        if _cell_occupied?(i+r, j)
+          backtrack unless dry_run
+          return 0
+        end
+        unless dry_run
+          _set_cell(i+r,j, cell)
+        end
+        @placed_pos << [i+r,j,cell]
+        score += 1
+      else
+         raise "invalid tile #{tile}"
+      end
+    end
+
+    @score += score unless dry_run
+    score
+  end
+
+  def show_tile(tile)
+    p tile
+  end
+
+  ## Improve this algorithm to find a better fit
+  def find_fitting_tile
+    # puts "*** Finding a fitting tile ***"
+    tiny_tiles = [HL[2], VL[2], SQ[2], BL[2], TL[2], BR[2], TR[2], DOT]
+    i,j,tile   = pos_exists?(tiny_tiles)
+    tile
+  end
+
+  def generate_tiles(n=Board1010::MAX_OPTIONS)
+    opt_tiles = []
+
+    n.times do |i|
+      r = rand(ALL_TILES.size)
+      tile =  ALL_TILES[r]
+      opt_tiles << tile
+    end
+
+    opt_tiles[0] = find_fitting_tile unless pos_exists?(opt_tiles)
+
+    opt_tiles
+  end
+
+
+  def show_tiles(all)
+    n = 1
+    all.each do |tile|
+      if tile
+        print " #{n}> "
+        show_tile(tile)
+        n += 1
+      end
+    end
+    n
+  end
+
+  def pos_exists?(tiles)
+    tiles.each do |tile|
+      @arr.each_with_index do |row,i|
+        row.each_with_index do |col,j|
+          score = (_cell_occupied?(i,j) ? 0 : place(tile,i,j,true))
+          return [i,j,tile] if score > 0
+        end
+      end
+    end
+    return false
+  end
+
+  def play
+    while (true) do
+      all = (@options.size > 0 ? @options : generate_tiles)
+      @options = all
+      while (all.size > 0) do
+        show
+        puts "\n#{'=' * 20}\nCurrent score = #{current_score}\n#{'=' * 20}\n"
+        show_tiles(all)
+        ended unless pos_exists?(all)
+        i,j = get_position
+        if all.size > 1
+          opt = get_selected_option(i,j)
+        else
+          opt = 1
+        end
+        tile = all[opt - 1]
+        new_score = place(tile,i,j)
+        if new_score < 1
+          puts " Cannot place the tile #{tile} at (#{i}, #{j})"
+        else
+          all.delete_at(opt - 1)
+          cleanup
+        end
+      end
+    end
+  end
+
+  def start
+    puts "Started"
+    @start_time = Time.now
+  end
+
+  def ended
+    puts "\n--- Game ended: no place for remaining tiles"
+    _over
+  end
+
+  def stop
+    puts "\n--- Game stopped: to be played later"
+    save
+    _over
+  end
+
+  def show
+    puts ""
+    print "   ";@cols.times{|j| print " #{j}"}
+    puts
+    @arr.each_with_index do |row,i|
+      print " #{i}["
+      row.each do |col|
+        if col == 0
+          print " ."
+        else
+          print " X"
+        end
+      end
+      puts " ]"
+    end
+    puts ""
+  end
+
+  def find_starting_pos(tile)
+    positions = []
+    @arr.each_with_index do |row,i|
+      row.each_with_index do |col,j|
+        score = place(tile,i,j,true)
+        positions << [i,j] if score > 0
+      end
+    end
+    positions
+  end
+
+  def neighbours(i,j)
+    count = 0
+
+    count += 1 if i > 0 && j > 0 && @arr[i-1][j-1] != 0
+    count += 1 if i > 0 && @arr[i-1][j  ] != 0
+    count += 1 if i > 0 && j < 9 && @arr[i-1][j+1] != 0
+
+    count += 1 if i < 9 && j > 0 && @arr[i+1][j-1] != 0
+    count += 1 if i < 9 && @arr[i+1][j  ] != 0
+    count += 1 if i < 9 && j < 9 && @arr[i+1][j+1] != 0
+
+    count += 1 if j > 0 && @arr[i  ][j-1] != 0
+    count += 1 if j < 9 && @arr[i  ][j+1] != 0
+
+    count
+  end
+
+  def best_starting_position(tile)
+    positions = find_starting_pos(tile)
+    neighbour_count = []
+    positions.each_with_index do |pos,k|
+      neighbour_count[k] = neighbours(*pos)
+    end
+    n = neighbour_count.index(neighbour_count.max)
+    n = 0
+    positions[n || 0]
+  end
+
+private
+
+  def _set_cell(i,j,val)
+    if _cell_valid?(i,j)
+      @arr[i][j] = val
+    end
+  end
+
+  def _cell_valid?(i,j)
+    @arr[i] && @arr[i][j]
+  end
+
+  def _cell_empty?(i,j)
+    _cell_valid?(i,j) && @arr[i][j] == @val
+  end
+
+  def _cell_occupied?(i,j)
+    not _cell_empty?(i,j)
+  end
+
+  def _over
+    stop_time = Time.now
+    puts "Total time elapsed = #{stop_time - @start_time} seconds"
+    puts "Score = #{@score}"
+    puts
+    raise "Over!"
+  end
+
+  def _filled_cells
+    count = 0
+    @arr.each do |row|
+       row.each do |col|
+         count += 1 if col > 0
+       end
+    end
+    count
+  end
+
+end
+
+if __FILE__ == $0
+  begin
+    puts "Total tiles = #{Board1010::ALL_TILES.size}"
+    b = Board1010.new
+    b.init
+    b.play
+  rescue
+    puts "> " + $!.to_s
+  end
+end
+
---
+++ game_1010.rb
@@ -0,0 +1,413 @@
+# gosu_1010.rb
+require "gosu"
+require "./board"
+
+class GameWindow < Gosu::Window
+  attr :clicked_x, :clicked_y
+
+  COLORS = {
+    :maroon => Gosu::Color.new(*[222, 217, 17, 152]),
+    :purple => Gosu::Color.new(*[245, 119, 47, 192]),
+    :brown  => Gosu::Color.new(*[226, 236, 85, 37]),
+    :grey   => Gosu::Color.new(*[62, 121, 218, 109]),
+    :pink   => Gosu::Color.new(*[193, 254, 9, 124]),
+    :bleau  => Gosu::Color.new(*[250, 5, 145, 245]),
+    :green  => Gosu::Color.new(*[248, 27, 184, 139]),
+  }
+
+  FONT_COLOR = Gosu::Color::RED
+  LINE_COLOR = Gosu::Color::BLUE
+
+  def initialize(random=false)
+    @width  = 480
+    @height = 480
+    super(@width,@height,{:fullscreen => false})
+    self.caption = "1010!"
+
+    @score_font = Gosu::Font.new(30)
+    @help_font  = Gosu::Font.new(20)
+
+    @board           = Board1010.new # Dependency injection?
+    @selected_option = 0
+    @selected_row    = @selected_col = -1
+    @clicked_x       = @clicked_y    = -1
+
+    @gap  = 2
+    @size = 32
+
+    @recording = []
+    @placed_positions = []
+    @rows_to_clean, @cols_to_clean = [],[]
+    @undo = @show_pos = @record = @playback = @help = false
+
+    @random = random
+    @count  = 0
+  end
+
+  def needs_cursor?
+    true
+  end
+
+  def draw_score
+    @score_font.draw("Score: #{@board.score}", 20, 360, 1, 1.0, 1.0, FONT_COLOR)
+  end
+
+  def draw_help
+    return unless @help
+    x,y = 340,340
+    @help_font.draw("ESC: abort",       x, y,    1, 1.0, 1.0, FONT_COLOR)
+    @help_font.draw("N: new game",      x, y+20, 1, 1.0, 1.0, FONT_COLOR)
+    @help_font.draw("S: save",          x, y+40, 1, 1.0, 1.0, FONT_COLOR)
+    @help_font.draw("D: random",        x, y+60, 1, 1.0, 1.0, FONT_COLOR)
+    @help_font.draw("Q: save and quit", x, y+80, 1, 1.0, 1.0, FONT_COLOR)
+    @help_font.draw("U: undo",          x,y+100, 1, 1.0, 1.0, FONT_COLOR)
+  end
+
+  def draw_background
+    draw_rect(0, 0, 0, @width, @height, Gosu::Color::WHITE)
+  end
+
+  def cell_color(value=0)
+    color = case value
+    when 0
+      COLORS[:grey]
+    when -1
+      COLORS[:bleau]
+    when -2
+      COLORS[:maroon]
+    when 1
+      COLORS[:green]
+    when 2
+      COLORS[:brown]
+    when 3
+      COLORS[:purple]
+    when 4
+      Gosu::Color::FUCHSIA
+    else
+      Gosu::Color::BLUE
+    end
+
+    color
+  end
+
+  def draw_cell(i,j,value)
+    x = @gap + (j * (@size + @gap))
+    y = @gap + (i * (@size + @gap))
+    z = 1
+
+    draw_sqr(x,y,z,@size,cell_color(value))
+  end
+
+  def clicked_xy
+    x,y = @clicked_x, @clicked_y  # these are from the latest click
+    @clicked_x = @clicked_y = -1  # so immediately reset the co-ordinates
+    [x,y]
+  end
+
+  def get_screen_position
+    x,y = clicked_xy
+    i,j = -1,-1
+
+    Board1010::MAX_COLS.times do |jj|
+      if (x >= @gap + (@size + @gap) * jj) && (x < @gap + (@size + @gap) * (jj + 1))
+        j = jj
+        break
+      end
+    end
+
+    Board1010::MAX_ROWS.times do |ii|
+      if (y >= @gap + (@size + @gap) * ii) && (y < @gap + (@size + @gap) * (ii + 1))
+        i = ii
+        break
+      end
+    end
+
+    [i,j]
+  end
+
+  def get_screen_option
+    x,y  = clicked_xy
+    opt  = 0
+    gap  = @gap / 2
+    size = @size / 2
+    (1..Board1010::MAX_OPTIONS).each do |i|
+      y1 = (22 * i)     + ((i - 1) * (size + gap) * 5)
+      y2 = (22 * (i+1)) + ( i      * (size + gap) * 5)
+      if (x > 360) && (y1 < y) && (y < y2)
+        opt = i
+        break
+      end
+    end
+    opt
+  end
+
+  def draw_option_tile(tile, pos, selected = false)
+    return unless (1..Board1010::MAX_OPTIONS).include?(pos)
+
+    gap  = @gap  / 2
+    size = @size / 2
+
+    x = 360
+    y = (22 * pos) + ((pos - 1) * (size + gap) * 5)
+    color = Gosu::Color::YELLOW
+    tile.each_with_index do |cell,i|
+      if cell.class == Array
+        cell.each_with_index do |ele, j|
+          if ele > 0
+            new_x = x + (size + gap) * j
+            new_y = y + (size + gap) * i
+            color = cell_color(ele) unless selected
+            draw_sqr(new_x,new_y,1,size,color)
+          end
+        end
+      elsif cell > 0
+        new_x = x + (size + gap) * i
+        color = cell_color(cell) unless selected
+        draw_sqr(new_x,y,1,size,color)
+      elsif cell < 0
+        new_y = y + (size + gap) * i
+        color = cell_color(cell) unless selected
+        draw_sqr(x,new_y,1,size,color)
+      end
+    end
+    draw_line(360, (11 * pos) + ((pos - 1) * (size + gap) * 5), LINE_COLOR,
+              460, (11 * pos) + ((pos - 1) * (size + gap) * 5), LINE_COLOR, 1)
+  end
+
+  def draw_sqr(x,y,z,length,color=Gosu::Color::WHITE)
+    draw_rect(x,y,z,length,length,color)
+  end
+
+  def draw_rect(x,y,z,width,height,color=Gosu::Color::WHITE)
+    c1=c2=c3=c4=color
+    x1,y1 = x,y
+    x2,y2 = x1+width,y1
+    x3,y3 = x1,y1+height
+    x4,y4 = x2,y3
+    Gosu::draw_quad(x1, y1, c1, x2, y2, c2, x3, y3, c3, x4, y4, c4, z, :default)
+  end
+
+  def draw_open_pos
+    return unless @show_pos
+
+    tile     = @option_tiles[@selected_option-1]
+    bpos      = @board.best_starting_position(tile)
+    positions = (bpos ? [bpos] : [])
+    positions.each do |pos|
+      i,j = pos
+      draw_cell(i,j,-2)
+    end
+  end
+
+  def draw_options
+    n = 1
+    @option_tiles.each_with_index do |tile,i|
+      if tile
+        draw_option_tile(tile, (i+1), @selected_option == (i+1))
+        n += 1
+      end
+    end
+    n
+  end
+
+  def draw_board
+    @board.arr.each_with_index do |row,i|
+      row.each_with_index do |val,j|
+        draw_cell(i,j,val)
+      end
+    end
+  end
+
+  def place_selected_tile
+    if @placed_positions.empty?
+      if @rows_to_clean.empty? && @cols_to_clean.empty?
+        @rows_to_clean, @cols_to_clean = @board.cleanup(true)
+      else
+        unless @rows_to_clean.empty?
+          i = @rows_to_clean.shift
+          Board1010::MAX_ROWS.times{|j| @board.cell = [i, j, 0] }
+        end
+
+        unless @cols_to_clean.empty?
+          j = @cols_to_clean.shift
+          Board1010::MAX_COLS.times{|i| @board.cell = [i, j, 0] }
+        end
+
+        sleep(0.24) unless @random
+      end
+    else
+      i,j,val = @placed_positions.shift
+      @board.cell = [i, j, val]
+      return
+    end
+
+    return unless ((@selected_option > 0) && (@selected_row >= 0) && (@selected_col >= 0))
+
+    unless @undo
+      @prev_score   = @board.score
+      @prev_arr     = Marshal.load(Marshal.dump(@board.arr))
+      @prev_option_tiles = Marshal.load(Marshal.dump(@option_tiles))
+    end
+    tile = @option_tiles[@selected_option - 1]
+    new_score = @board.place(tile, @selected_row, @selected_col, true)
+    if new_score > 0
+      @board.score += new_score
+      @placed_positions = @board.placed_pos
+      @option_tiles.delete_at(@selected_option - 1)
+    end
+    @selected_option = 0
+    @selected_row = @selected_col = -1
+  end
+
+  def draw_status
+    @count += 1
+    return unless @record
+
+    if (@count % 2 == 0)
+      x1,y1 = 20,400
+      x2,y2 = 40,400
+      x3,y3 = 30,410
+    else
+      x1,y1 = 20,400
+      x2,y2 = 40,400
+      x3,y3 = 30,390
+    end
+
+    z = 1
+    c1 = c2 = c3 = Gosu::Color::RED
+    draw_triangle(x1, y1, c1, x2, y2, c2, x3, y3, c3, z, :default)
+  end
+
+  def draw
+    draw_background
+    draw_board
+    draw_score
+    draw_help
+    draw_options
+    draw_open_pos
+    draw_status
+  end
+
+  def update
+    if @playback && @recording.size > 0
+      puts "playing back"
+      @selected_row, @selected_col, @selected_option, @option_tiles = @recording.shift
+      return
+    end
+
+    if @undo
+      @board.score   = @prev_score
+      @board.restore_arr(@prev_arr)
+      @option_tiles  = @prev_option_tiles
+
+      @undo = false
+    else
+      @option_tiles = @board.options
+      @option_tiles = @board.generate_tiles if @option_tiles.empty?
+      @board.ended unless @board.pos_exists?(@option_tiles)
+    end
+
+    if @random
+      @selected_option = rand(@option_tiles.size) + 1
+      tile = @option_tiles[@selected_option - 1]
+      position = @board.best_starting_position(tile)
+      if position
+        @selected_row, @selected_col = position
+      end
+    else
+      @selected_option = get_screen_option if @selected_option == 0
+      @selected_row, @selected_col = get_screen_position if @selected_row < 0 || @selected_col < 0
+
+      if @record
+        @recording << [@selected_row, @selected_col, @selected_option, @option_tiles]
+      end
+    end
+
+    place_selected_tile
+
+    @board.restore_options(@option_tiles)
+  end
+
+  def start
+    @start_time = Time.now
+    @random ? @board.init(0) : @board.init
+    self.show
+  end
+
+  def stop
+    stop_time = Time.now
+    puts "Total time played = #{(stop_time - @start_time).to_i} seconds"
+    puts "Score = #{@board.score}"
+    self.close
+  end
+
+  def button_up(id)
+    case (id)
+    when Gosu::MsLeft
+      @clicked_x = self.mouse_x
+      @clicked_y = self.mouse_y
+    when Gosu::MsRight
+      @show_pos = false
+    when Gosu::KbH
+      @help = false
+    end
+  end
+
+  def button_down(id)
+    case id
+    when Gosu::KbEscape
+      stop
+    when Gosu::KbA
+      # about
+    when Gosu::KbC
+      # cheat
+    when Gosu::KbD
+      @random = !@random
+    when Gosu::KbH
+      @help = true
+    when Gosu::KbN
+      # new
+      @board.init(0)
+    when Gosu::KbP
+      self.caption = "1010! (playback)"
+      @playback  = true
+      @board.init(0)
+    when Gosu::KbQ
+      @board.stop
+    when Gosu::KbR
+      @record = true
+      self.caption = "1010! (recording)"
+    when Gosu::KbS
+      @board.save
+    when Gosu::KbU
+      @undo = true
+    when Gosu::Kb1
+      @selected_option = 1
+    when Gosu::Kb2
+      @selected_option = 2
+    when Gosu::Kb3
+      @selected_option = 3
+    when Gosu::MsRight
+      @show_pos = true
+    when Gosu::MsLeft
+      # do nothing
+    else
+      puts "Button down pressed with id = #{id}"
+    end
+  end
+
+end
+
+if __FILE__ == $0
+  begin
+    win = GameWindow.new(ARGV[0])
+    win.start
+  rescue Exception => e
+    if RUBY_ENGINE == 'mruby'
+      raise e
+    else
+      puts "> " + $!.to_s
+    end
+  end
+end
+
